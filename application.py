c784e2e134129667ba88142ca63b362419ec199f
Mehr Sharma <mehrsharma@gmail.com>
2024-02-19 13:21:33 -0500

    more config stuff

--- application.py
+++ application.py
@@ -1,20 +1,20 @@
 from flask import Flask, request
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def hello_world():
     return 'Hello, World 0!'
 
-@app.route('/input_taker', methods=['POST'])
+@application.route('/input_taker', methods=['POST'])
 def say_my_name():
     data = request.get_json()
     name = data.get('name', '')
     return f"Hello, {name}!"
 
-@app.route('/hello_world_1')
+@application.route('/hello_world_1')
 def hello_world_1():
     return 'Hello, World 1!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
